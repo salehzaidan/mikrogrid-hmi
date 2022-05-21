c8a009767b81903cb7c507dde86b6139b965660d
Saleh Zaidan <salehzaidan64@gmail.com>
2022-05-21 11:40:08 +0700

    Create navbar and footer

---
+++ components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+  return (
+    <footer className="bg-gray-900 px-6 py-3 text-sm text-white">
+      <div className="container mx-auto">
+        Dikembangkan oleh{' '}
+        <div className="font-medium">
+          Lab Manajemen Energi Teknik Fisika ITB
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.tsx
@@ -0,0 +1,12 @@
+function Navbar() {
+  return (
+    <header className="bg-gray-900 px-6 py-3 text-sm text-white">
+      <div className="container mx-auto">
+        <h1 className="text-xl font-semibold">Sistem Mikrogrid</h1>
+        Lab Manajemen Energi Teknik Fisika ITB
+      </div>
+    </header>
+  )
+}
+
+export default Navbar
